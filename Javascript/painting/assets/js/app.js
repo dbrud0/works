960f24340c8c9f4d663bc302c6cd0cec02dcd847
dbrud0 <tlsrod0@gmail.com>
2021-08-18 18:28:40 +0900

    Update app.js

--- Javascript/painting/assets/js/app.js
+++ Javascript/painting/assets/js/app.js
@@ -8,7 +8,7 @@ const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
-const CANVAS_SIZE = 250;
+const CANVAS_SIZE = 200;
 
 
 
